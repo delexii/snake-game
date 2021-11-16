96529840cf79aa5ba64e5f491d03630f79659b89
delexii <buted@elslapm-404436.home>
2021-11-16 15:21:26 +0000

    refactor: image to render in the middle

--- core/src/com/snake/Snake.java
+++ core/src/com/snake/Snake.java
@@ -17,9 +17,9 @@ public class Snake extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 1, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, 190, 120);
 		batch.end();
 	}
 	
