211d28704f9a86e330fb136cbc1e2237d1b7a951
Leo Brown <leo_brown_1@hotmail.com>
2021-11-16 16:32:37 +0000

    Drawn a circle on screen

--- core/src/com/snake/Snake.java
+++ core/src/com/snake/Snake.java
@@ -1,26 +1,58 @@
 package com.snake;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Snake extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	ShapeRenderer shapeRenderer;
+	float circleX = 200;
+	float circleY = 100;
+
+
+
+//	public Snake () {
+//
+//	}
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		shapeRenderer = new ShapeRenderer();
+
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(0, 1, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 190, 120);
+//		batch.draw(img, 190, 120);
 		batch.end();
+
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(1, 1, 0, 1);
+        shapeRenderer.circle(circleX, circleY, 30);
+        shapeRenderer.end();
 	}
 	
 	@Override
