bfec41879245e9613d05d27b48dc6188b4bb912f
Bute <buted@elslapm-404436.home>
2021-11-16 14:40:45 +0000

    initialized empty project repo

---
+++ desktop/src/com/snake/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.snake.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.snake.Snake;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Snake(), config);
+	}
+}
