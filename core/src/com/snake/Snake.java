8f21ea91728dea94633a56e9ddbd0222d0216f58
Daniel <daniel.szabo09@gmail.com>
2021-11-17 15:07:03 +0000

    added EndGameScreen

--- core/src/com/snake/Snake.java
+++ core/src/com/snake/Snake.java
@@ -1,53 +1,37 @@
 package com.snake;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.TimeUtils;
-
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Snake extends ApplicationAdapter {
-	SpriteBatch batch;
+
+public class Snake extends Game {
 	Texture img;
 	ShapeRenderer shapeRenderer;
 	float circleX = 200;
 	float circleY = 100;
 
+public SpriteBatch batch;
+public BitmapFont font;
 
 
-//	public Snake () {
-//
-//	}
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		font = new BitmapFont();
 		img = new Texture("badlogic.jpg");
 		shapeRenderer = new ShapeRenderer();
+		setScreen(new EndGameScreen(this));
 
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(0, 0, 0, 1);
-		batch.begin();
-//		batch.draw(img, 190, 120);
-		batch.end();
 
 		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(1, 1, 0, 1);
@@ -66,11 +50,11 @@ public class Snake extends ApplicationAdapter {
 		if (circleY < 30) circleY = 30;
 		if (circleY > 1080 - 30) circleY = 1080 - 30;
 
-		//Break the app if touch the wall
-		if (circleX == 30) Gdx.app.exit();
-		if (circleX == 1920 - 30) Gdx.app.exit();
-		if (circleY == 30) Gdx.app.exit();
-		if (circleY == 1080 - 30) Gdx.app.exit();
+		//EndGameScreen when the snake touch the wall
+		if (circleX == 30) setScreen(new EndGameScreen(this));
+		if (circleX == 1920 - 30) setScreen(new EndGameScreen(this));
+		if (circleY == 30) setScreen(new EndGameScreen(this));
+		if (circleY == 1080 - 30) setScreen(new EndGameScreen(this));
 	}
 	
 	@Override
