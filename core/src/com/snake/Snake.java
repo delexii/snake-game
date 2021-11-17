121882b191c80014ed4b2c0b11311e016a8acf93
Leo Brown <leo_brown_1@hotmail.com>
2021-11-17 11:13:15 +0000

    added tail segment that moves with head

--- core/src/com/snake/Snake.java
+++ core/src/com/snake/Snake.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
@@ -25,8 +26,13 @@ public class Snake extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	ShapeRenderer shapeRenderer;
-	float circleX = 200;
-	float circleY = 100;
+	Pixmap pixmap;
+	float circleX = 1920/2;
+	float circleY = 1080/2;
+	float circle2X = 1920/2;
+	float circle2Y = 1080/2 -60;
+//	float width = 20;
+//	float height = 20;
 
 
 
@@ -39,30 +45,64 @@ public class Snake extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		shapeRenderer = new ShapeRenderer();
+//		Pixmap pixmap = new Pixmap(width, height, Pixmap.Format.RGBA8888);
 
 	}
 
 	@Override
 	public void render () {
+//		Pixmap pixmap = new Pixmap(width, height, Pixmap.Format.RGBA8888);
+//		pixmap.setColor(Color.BLACK);
+//		pixmap.fillCircle(x, y, r);
+//		Texture texture = new Texture(pixmap);
+
+
 		ScreenUtils.clear(0, 0, 0, 1);
 		batch.begin();
 //		batch.draw(img, 190, 120);
 		batch.end();
+
 
 		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(1, 1, 0, 1);
         shapeRenderer.circle(circleX, circleY, 30);
         shapeRenderer.end();
 
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) circleX -= git * Gdx.graphics.getDeltaTime();
-		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) circleX += 200 * Gdx.graphics.getDeltaTime();
-		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) circleY -= 200 * Gdx.graphics.getDeltaTime();
-		if (Gdx.input.isKeyPressed(Input.Keys.UP)) circleY += 200 * Gdx.graphics.getDeltaTime();
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.setColor(0, 1, 1, 1);
+		shapeRenderer.circle(circle2X, circle2Y, 30);
+		shapeRenderer.end();
+
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			circleX -= 200 * Gdx.graphics.getDeltaTime();
+			tailMove("LEFT");
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			circleX += 200 * Gdx.graphics.getDeltaTime();
+			tailMove("RIGHT");
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			circleY -= 200 * Gdx.graphics.getDeltaTime();
+			tailMove("UP");
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			circleY += 200 * Gdx.graphics.getDeltaTime();
+			tailMove("DOWN");
+		}
+	}
+
+	public void tailMove(String direction) {
+		if (direction == "LEFT")  circle2X -= 200 * Gdx.graphics.getDeltaTime();
+		if (direction == "RIGHT")  circle2X += 200 * Gdx.graphics.getDeltaTime();
+		if (direction == "UP")  circle2Y -= 200 * Gdx.graphics.getDeltaTime();
+		if (direction == "DOWN")  circle2Y += 200 * Gdx.graphics.getDeltaTime();
+
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		shapeRenderer.dispose();
 	}
 }
