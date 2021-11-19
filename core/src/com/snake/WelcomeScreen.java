922c8cbe8e3b8023135076cd5d868554b9b1c699
Daniel <daniel.szabo09@gmail.com>
2021-11-19 16:27:20 +0000

    Added buttons to WelcomeScreen

--- core/src/com/snake/WelcomeScreen.java
+++ core/src/com/snake/WelcomeScreen.java
@@ -4,47 +4,105 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScalingViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class WelcomeScreen extends ScreenAdapter {
 
     final  Snake game;
 
+    private Stage stage;
+
 
 
     public WelcomeScreen (Snake game) {
         this.game = game;
+
+        // create a stage and set up as an inputprocessor
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+    }
+
+
+    @Override
+    public void show() {
+        // create a table that fills the screen. Everything else will go inside the table
+        Table table = new Table();
+        table.setFillParent(true);
+        table.setDebug(false);
+        stage.addActor(table);
+
+        // Button skins
+        Skin skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
+
+        TextButton newGame = new TextButton("New Game", skin);
+        TextButton exit = new TextButton("Exit", skin);
+
+        //Add buttons to the table
+        table.add(newGame).prefHeight(100).prefWidth(300);
+        table.row().pad(10,0,10,0);
+        table.add(exit).prefHeight(100).prefWidth(300);
+
+        // create button listener
+        newGame.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.setScreen(new GameScreen(game));
+            }
+        });
+
+        exit.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
+
+
+
+
+
+//        Gdx.input.setInputProcessor(new InputAdapter() {
+//            @Override
+//            public boolean keyDown(int keyCode) {
+//                if(keyCode == Input.Keys.SPACE) {
+//                    game.setScreen(new GameScreen(game));
+//                } else if(keyCode == Input.Keys.ESCAPE){
+//                    Gdx.app.exit();
+//                }
+//                return true;
+//            }
+//        });
     }
 
 
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(0.53f,0.81f,0.92f,1 );
+//        ScreenUtils.clear(0,0,0,1 );
+        Gdx.gl.glClearColor(0f, 0f,0f,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f ));
+        stage.draw();
+
 
         game.batch.begin();
-        game.font.draw(game.batch, "WELCOME", 920, 800);
-        game.font.draw(game.batch, "to", 950, 760);
-        game.font.draw(game.batch, "HUNGRY SNAKE !!", 900, 720);
-        game.font.draw(game.batch, "Press Space to Start the game", 880, 600);
-        game.font.draw(game.batch, "Press Esc to Quit the game", 880, 570);
+//        game.font.draw(game.batch, "WELCOME", 920, 800);
+//        game.font.draw(game.batch, "to", 950, 760);
+//        game.font.draw(game.batch, "HUNGRY SNAKE !!", 900, 720);
+//        game.font.draw(game.batch, "Press Space to Start the game", 880, 600);
+//        game.font.draw(game.batch, "Press Esc to Quit the game", 880, 570);
         game.font.draw(game.batch, "COPYRIGHT 2021 Tiger Cubed Games", 1600, 20);
         game.batch.end();
 
-    }
-
-    @Override
-    public void show() {
-        Gdx.input.setInputProcessor(new InputAdapter() {
-            @Override
-            public boolean keyDown(int keyCode) {
-                if(keyCode == Input.Keys.SPACE) {
-                    game.setScreen(new GameScreen(game));
-                } else if(keyCode == Input.Keys.ESCAPE){
-                    Gdx.app.exit();
-                }
-                return true;
-            }
-        });
     }
 
 
@@ -55,6 +113,11 @@ public class WelcomeScreen extends ScreenAdapter {
 
     @Override
     public void dispose() {
+        stage.dispose();
 
+    }
+    @Override
+    public void resize(int width, int height) {
+      stage.getViewport().update(width, height, true);
     }
 }
