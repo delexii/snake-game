42f6f8515a4a67fb08eda1625a9ab711635376b2
Daniel <daniel.szabo09@gmail.com>
2021-11-21 19:38:34 +0000

    Added buttons and sign to EndGameScreen

--- core/src/com/snake/EndGameScreen.java
+++ core/src/com/snake/EndGameScreen.java
@@ -1,45 +1,92 @@
 package com.snake;
 
 import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class EndGameScreen extends ScreenAdapter {
 
     final  Snake game;
+    Rectangle gameOver;
+    Texture gameOverImage;
+    private Stage stage;
 
 
 
     public EndGameScreen(Snake game) {
         this.game = game;
+
+        gameOverImage = new Texture(Gdx.files.internal("game_over.png"));
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        gameOver = new Rectangle();
+        gameOver.x = 1920 / 2 - 388 / 2;
+        gameOver.y = 650;
+        gameOver.width = 388;
+        gameOver.height = 314;
+    }
+
+    @Override
+    public void show() {
+
+        Table table = new Table();
+        table.setFillParent(true);
+        table.setDebug(false);
+        stage.addActor(table);
+
+
+        Skin skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
+
+        TextButton tryAgain = new TextButton("Try Again", skin);
+        TextButton exit = new TextButton("Exit", skin);
+
+
+        table.row().pad(200, 0, 10, 0);
+        table.add(tryAgain).prefHeight(100).prefWidth(300);
+        table.row().pad(10, 0, 10, 0);
+        table.add(exit).prefHeight(100).prefWidth(300);
+
+
+        tryAgain.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.setScreen(new GameScreen(game));
+            }
+        });
+
+        exit.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Gdx.app.exit();
+            }
+        });
     }
 
 
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(0.83f, 0.13f, 0.18f, 1);
+    Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        game.batch.begin();
-        game.font.draw(game.batch, "Game Over !!", 950, 860);
-        game.font.draw(game.batch, "Do you want to play again ??", 900, 720);
-        game.font.draw(game.batch, "Press Space if Yes or ESC for Quit", 880, 550);
-        game.font.draw(game.batch, "COPYRIGHT 2021 Tiger Cubed Games", 1600, 20);
-        game.batch.end();
 
-    }
+    stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f ));
+    stage.draw();
 
-    @Override
-    public void show() {
-        Gdx.input.setInputProcessor(new InputAdapter() {
-            @Override
-            public boolean keyDown(int keyCode) {
-                if(keyCode == Input.Keys.SPACE) {
-                    game.setScreen(new GameScreen(game));
-                } else if(keyCode == Input.Keys.ESCAPE){
-                    Gdx.app.exit();
-                }
-                return true;
-            }
-       });
+    game.batch.begin();
+    game.batch.draw(gameOverImage, gameOver.x, gameOver.y, gameOver.width, gameOver.height);
+    game.font.draw(game.batch, "COPYRIGHT 2021 Tiger Cubed Games", 1600, 20);
+    game.batch.end();
+
     }
 
 
@@ -50,6 +97,6 @@ public class EndGameScreen extends ScreenAdapter {
 
     @Override
     public void dispose() {
-
+    stage.dispose();
     }
 }
--- core/src/com/snake/WelcomeScreen.java
+++ core/src/com/snake/WelcomeScreen.java
@@ -1,8 +1,6 @@
 package com.snake;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -11,8 +9,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.ScalingViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class WelcomeScreen extends ScreenAdapter {
@@ -65,28 +61,11 @@ public class WelcomeScreen extends ScreenAdapter {
                 Gdx.app.exit();
             }
         });
-
-
-
-
-
-//        Gdx.input.setInputProcessor(new InputAdapter() {
-//            @Override
-//            public boolean keyDown(int keyCode) {
-//                if(keyCode == Input.Keys.SPACE) {
-//                    game.setScreen(new GameScreen(game));
-//                } else if(keyCode == Input.Keys.ESCAPE){
-//                    Gdx.app.exit();
-//                }
-//                return true;
-//            }
-//        });
     }
 
 
     @Override
     public void render(float delta) {
-//        ScreenUtils.clear(0,0,0,1 );
         Gdx.gl.glClearColor(0f, 0f,0f,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
@@ -95,11 +74,6 @@ public class WelcomeScreen extends ScreenAdapter {
 
 
         game.batch.begin();
-//        game.font.draw(game.batch, "WELCOME", 920, 800);
-//        game.font.draw(game.batch, "to", 950, 760);
-//        game.font.draw(game.batch, "HUNGRY SNAKE !!", 900, 720);
-//        game.font.draw(game.batch, "Press Space to Start the game", 880, 600);
-//        game.font.draw(game.batch, "Press Esc to Quit the game", 880, 570);
         game.font.draw(game.batch, "COPYRIGHT 2021 Tiger Cubed Games", 1600, 20);
         game.batch.end();
 
